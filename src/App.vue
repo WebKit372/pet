ad0eae27fd9c42407fb47ef0a0733733445d8dcd
WebKit372 <69135250+WebKit372@users.noreply.github.com>
2024-03-24 23:38:56 +0300

    wefwe

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Title from './components/Title.vue';
+const greeting = "Услуги Росавиации"
+</script>
+
+<template>
+  <div class="main p-20">
+    <Title :title="greeting"/>
+  </div>
+</template>
---
+++ src/components/Title.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const props = defineProps({
+  title: String,
+})
+</script>
+
+<template>
+ <div class="bg-white text-center rounded-2xl">
+  <p class="font-sans text-lg font-semibold">{{title}}</p>
+ </div>
+</template>
