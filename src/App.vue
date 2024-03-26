d443f9e2d019940b6621a16b5fef8007927da048
Tamir Erdyneev <tamir431104@gmail.com>
2024-03-26 11:06:49 +0300

    Исправил flex в App

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ const greeting = "Услуги Росавиации"
 </script>
 
 <template>
-  <div class="main p-20 flex flex-col flex-wrap content-center">
+  <div class="main p-20 flex flex-col items-center">
     <Title :title="greeting"/>
     <ServiceList class="mt-5"/>
   </div>
