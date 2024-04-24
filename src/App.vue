e87416af5bf013aa96e6668db0cbc33709af7901
WebKit372 <69135250+WebKit372@users.noreply.github.com>
2024-04-24 21:31:06 +0300

    Добавил абсолют для хедера

--- src/App.vue
+++ src/App.vue
@@ -11,26 +11,27 @@ watch(route, (to , from) => {
     if(!from.path.split('/')[1]){
       back.value = false;
       history.value = false;
+      greeting.value = 'Услуги Росавиации'  
       return
     }
     else if(from.path.split('/')[1] === 'service'){
       history.value = true
     }
     else{
-      history.value = false      
+      history.value = false 
+      greeting.value = 'История'     
     }
     back.value = true;    
   })
 function toHome(){
   router.go(-1)
-  greeting.value = 'Услуги Росавиации'
 }
 </script>
 
 <template>
   <div class="main p-0 flex flex-col items-center md:p-20 sm:p-10">
-    <div class="flex flex-col items-center w-full max-w-5xl">
-      <div v-if="back" class="self-start">
+    <div class="flex flex-col items-center w-full max-w-5xl relative mt-20">
+      <div v-if="back" class="self-start absolute -top-20">
         <img @click="toHome" class="max-w-12 max-h-12 mb-5 ease-in-out duration-300 hover:opacity-50 hover:scale-125 hover:cursor-pointer" src="./assets/return-back-button-svgrepo-com.svg"/>
       </div>
       <Title :title="greeting"/>
--- src/components/History.vue
+++ src/components/History.vue
@@ -9,7 +9,6 @@ const srv = {
     "statusCode": "Статус" 
 }
 import { useRoute, useRouter } from 'vue-router';
-const route = useRoute();
 const router = useRouter();
 function toService(item){
     router.push(`/service/${item.serviceCode}`)
@@ -18,10 +17,6 @@ function toService(item){
 <template>
     <ul class="bg-white text-center rounded-2xl pt-5 max-w-5xl w-full mt-5">
         <li class="font-semibold  border-slate-300 border-b pt-6 pb-10">
-        <Service :srv="srv"/>
-        </li>
-        <li  v-for="(item) in uslugi" @click="toService(item)" class="border-solid border-slate-300 border-b py-4 ease-in-out duration-300  hover:bg-gray-300 last-of-type:rounded-b-2xl cursor-pointer">
-            <Service :srv="item"/>
         </li>
     </ul>
 </template>
